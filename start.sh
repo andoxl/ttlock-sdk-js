9a4e160433df75b142556a411d5ead103ddb1f70
andoxl <andoxl@proton.me>
2025-12-29 23:39:09 +0100

    Update start.sh

--- start.sh
+++ start.sh
@@ -1,15 +1,5 @@
 #!/usr/bin/env sh
 
-export GATEWAY="none"
-export GATEWAY_HOST=""
-export GATEWAY_PORT=""
-export GATEWAY_KEY=""
-export GATEWAY_USER=""
-export GATEWAY_PASS=""
-
-export NOBLE_WEBSOCKET=1
-export WEBSOCKET_DEBUG=1
-
 /sbin/openrc
 touch /run/openrc/softlevel
 
